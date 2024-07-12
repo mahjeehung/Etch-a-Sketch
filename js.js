495381c86bb74eb6dcca73f695ee8ba789989037
mahjeehung <151704278+mahjeehung@users.noreply.github.com>
2024-07-12 20:34:53 +0800

    Add 1/2 hover

--- js.js
+++ js.js
@@ -1,9 +1,21 @@
 const container = document.getElementsByClassName("container")
 
-document.addEventListener('DOMContentLoaded',function (e){
+
+// document.addEventListener('DOMContentLoaded',function (e){
     for (let i = 0; i < 255; i++) {
     const div = document.createElement('div')
     div.className = 'div'
     document.getElementsByClassName('container')[0].appendChild(div)
+
+    let DIV = Array.from(document.getElementsByClassName("div"));
+function divMouseEnter(e){
+    e.target.style.backgroundColor="blue";
+  }
+
+for (var k = 0; k < DIV.length; k++) {
+    DIV[k].addEventListener('mouseenter', divMouseEnter);
+}
     }
-})
+// })
+
+
