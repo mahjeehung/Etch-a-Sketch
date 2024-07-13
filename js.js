e4210bf4d3721246145562d815ba859a0804656f
mahjeehung <151704278+mahjeehung@users.noreply.github.com>
2024-07-13 16:27:43 +0800

    Grid width height

--- js.js
+++ js.js
@@ -1,26 +1,29 @@
-const container = document.getElementsByClassName("container")
+const container = document.getElementsByClassName("container");
 
-
-// document.addEventListener('DOMContentLoaded',function (e){
-    for (let i = 0; i < 255; i++) {
+(function resetGrid(x){
+  let grid = x;
+  
+  for (let i = 0; i < grid**2; i++) {
     const div = document.createElement('div')
     div.className = 'div'
-    document.getElementsByClassName('container')[0].appendChild(div)
-
+    div.style.width=`${(480/(grid+1))}px`;
+    div.style.height=`${(480/(grid+1))}px`;
+    document.getElementsByClassName('container')[0].appendChild(div)}
+    
     let DIV = Array.from(document.getElementsByClassName("div"));
-function divMouseEnter(e){
+
+  function divMouseEnter(e){
     e.target.style.backgroundColor="blue";
   }
-function divMouseLeave(e){
-    e.target.style.backgroundColor="white";
+  function divMouseLeave(e){
+    e.target.style.backgroundColor="yellow";
   }
 
-for (var k = 0; k < DIV.length; k++) {
+  let donald = Math.floor((window.innerWidth-50)/grid);
+  for (var k = 0; k < DIV.length; k++) {
     DIV[k].addEventListener('mouseenter', divMouseEnter);
     DIV[k].addEventListener('mouseleave', divMouseLeave);
-
+    // DIV[k].style.minHeight=`${donald}px`;
+    // DIV[k].style.minWidth=`${donald}px`;
 }
-    }
-// })
-
-
+})(16);
