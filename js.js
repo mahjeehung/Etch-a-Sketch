ea008fdb1edaec0323cc7844ec7a3d39025be223
mahjeehung <151704278+mahjeehung@users.noreply.github.com>
2024-07-13 18:33:28 +0800

    Add btn & function
    Added btn and function, but container size needs work

--- js.js
+++ js.js
@@ -1,13 +1,11 @@
 const container = document.getElementsByClassName("container");
-
-(function resetGrid(x){
-  let grid = x;
-  
-  for (let i = 0; i < grid**2; i++) {
+const btn = document.getElementById('btn');
+function resetGrid(x){
+    for (let i = 0; i < x**2; i++) {
     const div = document.createElement('div')
     div.className = 'div'
-    div.style.width=`${(480/(grid+1))}px`;
-    div.style.height=`${(480/(grid+1))}px`;
+    div.style.width=`${(480/(x+1))}px`;
+    div.style.height=`${(480/(x+1))}px`;
     document.getElementsByClassName('container')[0].appendChild(div)}
     
     let DIV = Array.from(document.getElementsByClassName("div"));
@@ -19,11 +17,30 @@ const container = document.getElementsByClassName("container");
     e.target.style.backgroundColor="yellow";
   }
 
-  let donald = Math.floor((window.innerWidth-50)/grid);
+  let donald = Math.floor((window.innerWidth-50)/x);
   for (var k = 0; k < DIV.length; k++) {
     DIV[k].addEventListener('mouseenter', divMouseEnter);
     DIV[k].addEventListener('mouseleave', divMouseLeave);
     // DIV[k].style.minHeight=`${donald}px`;
     // DIV[k].style.minWidth=`${donald}px`;
 }
-})(16);
+}
+
+document.addEventListener('DOMContentLoaded',resetGrid(16));
+
+function removeElementsByClass(y){
+  const elements = document.getElementsByClassName(y);
+  while(elements.length > 0){
+      elements[0].parentNode.removeChild(elements[0]);
+  }
+}
+
+function newGrid(){
+  let x = prompt ('Please enter number for new grid','1');
+  if (x>100||x<1){alert('please enter a number between 1-100')}
+  removeElementsByClass('div');
+  resetGrid(x);
+}
+
+btn.addEventListener('click', newGrid)
+
